a5c68e558317e486affb923a249cc158cecc8417
Ryan Bussert <ryanbussert@Ryans-MacBook-Pro.local>
2023-10-21 22:26:31 -0400

    cosine

--- methods/tools/toolsTA.js
+++ methods/tools/toolsTA.js
@@ -1,4 +1,5 @@
 var toolsTA = {
+  
     getEMA : function (data, period) {
         const smoothingFactor = 2 / (period + 1);
       
@@ -28,6 +29,38 @@ var toolsTA = {
         const atrMin = this.calculateATR(highPrices, lowPrices, closePrices, volmin);
         const atrMax = this.calculateATR(highPrices, lowPrices, closePrices, volmax);
         return atrMin[atrMin.length - 1] > atrMax[atrMax.length - 1];
+      },
+
+      getRSIS: function (closePrices, period) {
+        let gain = 0, loss = 0;
+        let rsis = [0]; // Assuming RSI is 0 for the first data point
+        for (let i = 1; i < closePrices.length; i++) {
+          const difference = closePrices[i] - closePrices[i - 1];
+          if (difference >= 0) {
+            gain = (gain * (period - 1) + difference) / period;
+            loss = loss * (period - 1) / period;
+          } else {
+            gain = gain * (period - 1) / period;
+            loss = (loss * (period - 1) - difference) / period;
+          }
+          const rs = gain / loss;
+          const rsi = 100 - (100 / (1 + rs));
+          rsis.push(rsi);
+        }
+        return rsis;
+      },
+
+      calculateATR: function (highPrices, lowPrices, closePrices, period) {
+        let atr = [0];
+        for (let i = 1; i < closePrices.length; i++) {
+          const high = highPrices[i];
+          const low = lowPrices[i];
+          const closePrev = closePrices[i - 1];
+          const tr = Math.max(high - low, Math.abs(high - closePrev), Math.abs(low - closePrev));
+          const atrValue = ((atr[atr.length - 1] * (period - 1)) + tr) / period;
+          atr.push(atrValue);
+        }
+        return atr;
       },
 }
 
